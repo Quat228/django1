4a17afec3d399b64167a6517ff8e64de8c1c56ce
Quat228 <zharkulov04@mail.ru>
2023-03-06 22:36:19 +0600

    third_hw

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
-from .models import Item
+from . import models
 
 
-def index(request):
-    items = Item.objects.all()
-    return HttpResponse(items)
+def list_item(request):
+    items = models.Item.objects.all()
+    context = {
+        "items": items,
+    }
+    if request.method == "POST":
+        return redirect(reverse("detail_item", kwargs={"item_id": request.POST.get("item_id")}))
+    return render(request, "list_item.html", context)
+
+
+def detail_item(request, item_id):
+    try:
+        item = models.Item.objects.get(id=item_id)
+    except Exception:
+        print("Нет такого айди")
+        return redirect(reverse("list_item"))
+    return render(request, "detail_item.html", {"item": item})
+
+
+
+
